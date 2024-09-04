ac94b678ccf3615c048c71831b1f7f7a67b69cb8
VaccalluzzoAlan <135074932+VaccalluzzoAlan@users.noreply.github.com>
2024-09-04 20:58:56 -0300

    Cambios a clases, prueba de NX

--- cmhmr.py
+++ cmhmr.py
@@ -29,8 +29,9 @@ class Grafo:
     def get(self, nombre):
         return self.vertices.get(nombre)
 
-    def agregar_distrito(self, nombre : str, distrito : Distrito):
-        if distrito not in self.vertices.items():
+    def agregar_distrito(self, nombre : str):
+        if nombre not in self.vertices:
+            distrito = Distrito(nombre)
             self.vertices[nombre] = distrito
 
     def agregar_camino(self, origen, destino, distancia):
@@ -78,23 +79,27 @@ class ColaFIFO:
         return len(self.cola) == 0
     
 class SistemaVentas:
-    def __init__(self):
+    def __init__(self, localidad : str = "Flores"):
         self.productos : dict[str, Producto] = {}
         self.clientes : dict[str, Cliente] = {}
         self.grafo_distritos = Grafo()
         self.ordenes = ColaFIFO()
+        self.local = localidad
+
+        self.agregar_distrito(self.local)
 
     def agregar_producto(self, nombre, precio):
-        producto = Producto(nombre, precio)
-        self.productos[nombre] = producto
+        if nombre not in self.productos:
+            producto = Producto(nombre, precio)
+            self.productos[nombre] = producto
 
     def agregar_cliente(self, dni, nombre):
-        cliente = Cliente(dni, nombre)
-        self.clientes[dni] = cliente
+        if dni not in self.clientes:
+            cliente = Cliente(dni, nombre)
+            self.clientes[dni] = cliente
 
     def agregar_distrito(self, nombre):
-        distrito = Distrito(nombre)
-        self.grafo_distritos.agregar_distrito(nombre, distrito)
+        self.grafo_distritos.agregar_distrito(nombre)
 
     def agregar_ruta(self, origen, destino, distancia):
         self.grafo_distritos.agregar_camino(origen, destino, distancia)
@@ -122,14 +127,35 @@ class SistemaVentas:
         self.ordenes.encolar(orden)
         # print(f"Orden para {producto.nombre} de {cliente.nombre} en {distrito.nombre} ha sido creada.")
 
-    def procesar_ordenes(self):
-        while not self.ordenes.esta_vacia():
+    def procesar_orden(self):
+        if self.ordenes.esta_vacia():
+            return None
+        else:
             orden : Orden = self.ordenes.desencolar()
-            distancia = self.grafo_distritos.dijkstra('Flores', orden.distrito.nombre)
-            # print(f"Procesando orden de {orden.producto.nombre} para {orden.cliente.nombre}.")
-            # print(f"Ruta más corta a {orden.distrito.nombre} es de {distancia} km.")
+            return orden
+        
+    def ruta_mas_corta(self, destino, inicio = None):
+        if not inicio:
+            inicio = self.local
+        distancia = self.grafo_distritos.dijkstra(inicio, destino)
+        return distancia
+    
+    def procesar_ordenes(self):
+        ordenes = []
+        distancias = {}
+        while not self.ordenes.esta_vacia():
+            orden : Orden = self.procesar_orden()
+            if orden:
+                ordenes.append(orden)
+                distancia = self.ruta_mas_corta(orden.distrito.nombre)
+                distancias[orden.distrito.nombre] = distancia
+                # print(f"Procesando orden de {orden.producto.nombre} para {orden.cliente.nombre}.")
+                # print(f"Ruta más corta a {orden.distrito.nombre} es de {distancia} km.")
+            
+        return ordenes, distancias
 
 def testSistemaVentas():
+
     # Crear el sistema
     sistema = SistemaVentas()
 
@@ -151,4 +177,9 @@ def testSistemaVentas():
 
     # Realizar y procesar órdenes
     sistema.realizar_orden("12345678", "Laptop", "Palermo")
-    sistema.procesar_ordenes()
+    print(sistema.procesar_ordenes())
+
+    return sistema
+
+if __name__ == "__main__":
+    testSistemaVentas()
--- guiVentas.py
+++ guiVentas.py
@@ -11,8 +11,31 @@ class GUI:
     def __init__(self):
         pass
 
+def test():
+    sistema = cmhmr.testSistemaVentas()
+    distritos = [distr.nombre for distr in sistema.grafo_distritos.vertices.values()]
+    print([f"{nombre}: {distr.conexiones}" for nombre, distr in sistema.grafo_distritos.vertices.items()])
+    
+    print("--------------------------")
+
+    G = nx.Graph()
+    G.add_nodes_from(distritos)
+    
+    aristas = []
+    for distrNombre in distritos:
+        distr : cmhmr.Distrito = sistema.grafo_distritos.get(distrNombre)
+        for conex, dist in distr.conexiones.items():
+            arista = (distrNombre, conex, dist)
+            aristaReves = (conex, distrNombre, dist)
+            if arista not in aristas and aristaReves not in aristas:
+                aristas.append(arista)
+
+    G.add_weighted_edges_from(aristas)
+    
+    print(G.edges(data=True))
+
 def main():
-    print("!!")
+    test()
 
 if __name__ == "__main__":
     main()
